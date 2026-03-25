8be1a775e10b4555a6b442af4848f67ba6e080f7
ppcjobber <ppcjobber@gmail.com>
2026-03-25 23:26:25 +0000

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function SignUpPage() {
-  return (
-    <div style={{ minHeight:"100vh", display:"flex", alignItems:"center", justifyContent:"center", position:"relative", zIndex:1 }}>
-      <SignUp />
-    </div>
-  );
-}
