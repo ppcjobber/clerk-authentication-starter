817d5b268a7cbfbae5f245aee118caf2d07c628f
ppcjobber <ppcjobber@gmail.com>
2026-03-25 23:27:12 +0000

    Delete route.ts

--- route.ts
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function SignInPage() {
-  return (
-    <div style={{ minHeight:"100vh", display:"flex", alignItems:"center", justifyContent:"center", position:"relative", zIndex:1 }}>
-      <SignIn />
-    </div>
-  );
-}
